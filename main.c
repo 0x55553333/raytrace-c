fe2f667a528dba945c15e3a4778d3220c5361892
Rui-Jie Fang <rui.jie.fang@gmail.com>
2019-01-29 23:20:15 -0500

    More

--- main.c
+++ main.c
@@ -1,29 +1,120 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "pgm.h"
+#include <math.h>
 
+#define SWAP(a, b, t) \
+{ t c = a; a = b; b = c; }
 
-/**
- * \brief Uses Bresenham’s Line Drawing Algorithm to draw a line from (x0,y0) to (x1, y1)
- * \param x0 the x-component of the start vector
- * \param y0 the y-component of the start vector
- * \param x1 the x-component of the end vector
- * \param y1 the y-component of the end vector
- * \param img a PGM image
- * \param color a color (in grayscale)
- */
-void line(long x0, long y0, long x1, long y1, PGM_t *img, unsigned char color)
+struct _LinearFunction;
+
+typedef long (*LinearFunctionEvaluator_t)(struct _LinearFunction, long x);
+
+typedef struct _LinearFunction (*GenerateLinearFunction_t)(struct _LinearFunction);
+
+typedef struct _LinearFunction {
+    long m, b;
+    LinearFunctionEvaluator_t eval;
+    GenerateLinearFunction_t transpose;
+} LinearFunction_t;
+
+long evaluateLinearFunction(struct _LinearFunction lf, long x)
 {
-    float t;
-    for (t = 0; t < 1.; t += 0.001) {
-        long x = (long) (x0 + (x1 - x0) * t);
-        long y = (long) (y0 + (y1 - y0) * t);
-        img->setPoint(img, (size_t)x, (size_t)y, color);
+    return x * lf.m + lf.b;
+}
+
+struct _LinearFunction transposeLinearFunction(struct _LinearFunction f1)
+{
+    // y = mx + b
+    // x=(y - b)/m=1/m*y - b/m
+    struct _LinearFunction f2 = {.b = (long) (-(float) f1.b / (float) f1.m),
+            .m = (long) ((float) 1 / (float) f1.m)};
+    return f2;
+}
+
+int pointAbove(LinearFunction_t f, long x0, long y0)
+{
+    return f.eval(f, x0) > y0;
+}
+
+int pointBelow(LinearFunction_t f, long x0, long y0)
+{
+    return f.eval(f, x0) < y0;
+}
+
+int pointOnLine(LinearFunction_t f, long x0, long y0)
+{
+    return f.eval(f, x0) == y0;
+}
+
+struct _LinearFunction newLinearFunction(long m, long b)
+{
+    return (struct _LinearFunction) {m, b, &evaluateLinearFunction, &transposeLinearFunction};
+}
+
+struct _LinearFunction newVector2(long x0, long y0, long x1, long y1)
+{
+    return newLinearFunction((y0 - y1) / (x0 - x1), y0 - (y0 - y1) / (x0 - x1) * x0);
+}
+
+void fillNeighbors(PGM_t *img, long x, long y, unsigned char color)
+{
+    static const long nh[4][2] = {{0,  1},
+                                  {1,  0},
+                                  {0,  -1},
+                                  {-1, 0}};
+    for (int i = 0; i < 4; ++i) {
+        long xt = nh[i][0] + x, yt = nh[i][1] + y;
+        if (xt >= 0 && xt < img->height && yt >= 0 && yt < img->width) {
+            img->setPoint(img, (size_t) xt, (size_t) yt, color);
+        }
     }
 }
 
+void line(long x0, long y0, long x1, long y1, PGM_t *image, unsigned char color)
+{
+    int steep = 0;
+    if (labs(x0 - x1) < labs(y0 - y1)) {
+        SWAP(x0, y0, long);
+        SWAP(x1, y1, long);
+        steep = 1;
+    }
+    if (x0 > x1) {
+        SWAP(x0, x1, long);
+        SWAP(y0, y1, long);
+    }
+    long dx = x1 - x0;
+    long dy = y1 - y0;
+    long derror2 = labs(dy) * 2;
+    long error2 = 0;
+    long y = y0;
+    for (long x = x0; x <= x1; x++) {
+        if (steep) {
+            image->setPoint(image, (size_t) y, (size_t) x, color);
+        } else {
+            image->setPoint(image, (size_t) x, (size_t) y, color);
+        }
+        error2 += derror2;
+        if (error2 > dx) {
+            y += (y1 > y0 ? 1 : -1);
+            error2 -= dx * 2;
+        }
+    }
+}
+
+
 void triangleShape(long x0, long y0, long x1, long y1, long x2, long y2, PGM_t *img, unsigned char color)
 {
+    /* sort from left to right by pairwise comparison */
+    if (x1 < x0 && x1 < x2) {
+        SWAP(x0, x1, long); SWAP(y0, y1, long);
+    }
+    if (x2 < x0 && x2 < x1) {
+        SWAP(x0, x2, long); SWAP(y0, y2, long);
+    }
+    if (x1 > x0 && x1 > x2) {
+        SWAP(x1, x2, long); SWAP(y1, y2, long);
+    }
     line(x0, y0, x1, y1, img, color);
     line(x1, y1, x2, y2, img, color);
     line(x2, y2, x0, y0, img, color);
@@ -53,21 +144,12 @@ void copyPGMFile(int argc, char **argv)
 int main(int argc, char **argv)
 {
     PGM_t *img = newPGM("Line Plot", 100UL, 100UL, 255);
-    for(size_t i = 0; i < 100UL; ++i)
-        for(size_t j = 0; j < 100UL; ++j)
+    for (size_t i = 0; i < 100UL; ++i)
+        for (size_t j = 0; j < 100UL; ++j)
             img->setPoint(img, i, j, 255);
 
-    triangleShape(33, 33, 88, 88, 0, 60, img, 0);
+    triangleShape(10, 10, 30, 20, 50, 50, img, 0);
     FILE *fp = fopen("./lineplot.pgm", "w");
-    for(size_t col = 0; col < img->width; ++col) {
-        int paint = 0;
-        for (size_t row = 0; row < img->height; ++row) {
-            if (img->getPoint(img, row, col) == 0) paint = 1;
-            if (paint == 1 && img->getPoint(img, row, col) == 255) paint = 2;
-            if (paint == 2 && img->getPoint(img, row, col) == 0) break;
-            if (paint == 2) img->setPoint(img, row, col, 0);
-        }
-    }
     img->write(img, fp);
     img->free(img);
     fclose(fp);
